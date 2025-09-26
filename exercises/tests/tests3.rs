4be5d792145e0eafbdff34595f6ace38d8ec307d
antheazheng <18883381598@163.com>
2025-09-26 20:55:52 +0800

    update

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -7,7 +7,6 @@
 // Execute `rustlings hint tests3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
@@ -19,11 +18,13 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        let is_true = is_even(10);
+        assert!(is_true);
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        let is_false= is_even(9);
+        assert!(!is_false);
     }
 }
